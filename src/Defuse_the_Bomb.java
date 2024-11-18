11cb25cdf4b4f689b271ec44a314ebf5a0ddac70
Raju <md.omarali746@gmail.com>
2024-11-18 11:09:59 +0600

    First commit

--- src/Defuse_the_Bomb.java
+++ src/Defuse_the_Bomb.java
@@ -1,2 +1,46 @@
+import java.util.Arrays;
+
 public class Defuse_the_Bomb {
+    public static int[] decrypt(int[] code, int k){
+
+        int n=code.length;
+        int[] result=new int[n];
+        if(k==0){
+            Arrays.fill(result, 0);
+            return result;
+        }
+        if (k > 0) {
+            // For k > 0, sum the next k elements
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n]; // Wrap around using modulo
+                }
+                result[i] = sum;
+            }
+        } else {
+            // For k < 0, sum the previous k elements
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= -k; j++) {
+                    sum += code[(i - j + n) % n]; // Wrap around using modulo
+                }
+                result[i] = sum;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] code={5,7,1,4};
+        int k=3;
+        System.out.println(Arrays.toString(decrypt(code,k)));
+
+    }
 }
+
+
+
+
