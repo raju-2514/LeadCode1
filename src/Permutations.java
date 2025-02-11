c2a499f331dc9c58b8d2d2a39fb4b6cf40546c12
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-02-11 23:46:29 +0600

    Add files via upload

---
+++ src/Permutations.java
@@ -0,0 +1,34 @@
+package February;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
+        return result;
+    }
+
+    private static void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] nums, boolean[] used) {
+        if (tempList.size() == nums.length) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            tempList.add(nums[i]);
+            backtrack(result, tempList, nums, used);
+            used[i] = false;
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> permutations = permute(nums);
+        System.out.println(permutations);
+    }
+}
