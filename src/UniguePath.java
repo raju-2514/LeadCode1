266d89195bf1d513d9c75961872f170ac51f1230
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-02-28 15:26:17 +0600

    Add files via upload

--- src/UniguePath.java
+++ src/UniguePath.java
@@ -1,41 +1,28 @@
 package February;
 
 public class UniguePath {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-
-        // If the start or end cell has an obstacle, there is no path
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
-            return 0;
-        }
-
+    public static int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        dp[0][0] = 1;
 
-        // Fill the first column
-        for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0] == 0) {
-                dp[i][0] = dp[i - 1][0];
-            }
+        // Fill the first row and first column with 1s
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
         }
 
-        // Fill the first row
-        for (int j = 1; j < n; j++) {
-            if (obstacleGrid[0][j] == 0) {
-                dp[0][j] = dp[0][j - 1];
-            }
-        }
-
-        // Fill the rest of the dp table
+        // Fill the rest of the grid
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
         return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3,2));
     }
 }
