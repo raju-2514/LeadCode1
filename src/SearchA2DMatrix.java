f710c64565ffa2c5be6bb8202fa9332d329ed96b
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-03-08 23:35:54 +0600

    Add files via upload

---
+++ src/SearchA2DMatrix.java
@@ -0,0 +1,33 @@
+package March;
+
+public class SearchA2DMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target = 3;
+        System.out.println(searchMatrix(matrix,target));
+    }
+}
