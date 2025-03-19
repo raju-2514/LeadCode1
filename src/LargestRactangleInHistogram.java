3fae09c0ba8d870e7493439461c956e1f64ac374
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-03-19 17:47:42 +0600

    Add files via upload

---
+++ src/LargestRactangleInHistogram.java
@@ -0,0 +1,29 @@
+package March;
+
+import java.util.Stack;
+
+public class LargestRactangleInHistogram {
+    public static int heightRectangle(int[] heights){
+
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+                 }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={2,1,5,6,2,3};
+        System.out.println(heightRectangle(arr));
+    }
+}
