193e0cb8ec07deb9230a779bdb792e8c9544992a
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-06-22 01:01:36 +0600

    Add files via upload

--- src/Solution.java
+++ src/Solution.java
@@ -1,57 +1,29 @@
-package April.ConstructBinaryTreeFromInorderAndPreorder;
-
-import java.util.HashMap;
-import java.util.Map;
+package June.SumRootToLeaf;
 
 public class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> inOrderIndexMap = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-            inOrderIndexMap.put(inorder[i], i);
-        }
-        return splitTree(preorder, inOrderIndexMap, 0, 0, inorder.length - 1);
-
+    public int sumNums(Node root) {
+        return dfs(root, 0);
     }
 
+    public int dfs(Node node,int current){
+        if (node==null){return 0;}
+        current=current*10+node.val;
 
-    private TreeNode splitTree(int[] preorder, Map<Integer, Integer> integerIntegerMap,
-                               int rootIndex, int left, int right) {
-        TreeNode root = new TreeNode(preorder[rootIndex]);
-        int mid = integerIntegerMap.get(preorder[rootIndex]);
-
-        if (mid > left) {
-            root.left = splitTree(preorder, integerIntegerMap,
-                    rootIndex + 1, left, mid - 1);
+        if(node.left==null && node.right==null){
+            return current;
         }
-
-        if (mid < right) {
-            root.right = splitTree(preorder, integerIntegerMap, rootIndex + mid - left + 1,
-                    mid - 1, right);
-        }
-        return root;
-
-    }
-
-    public static void printInorder(TreeNode root) {
-        if (root == null) return;
-        printInorder(root.left);
-        System.out.print(root.val + " ");
-        printInorder(root.right);
+        return dfs(node.left,current)+dfs(node.right,current);
     }
 
 
     public static void main(String[] args) {
-        // Sample input
-        int[] preorder = {3, 9, 20, 15, 7};
-        int[] inorder = {9, 3, 15, 20, 7};
+        Node root = new Node(4);
+        root.left = new Node(9);
+        root.right = new Node(0);
+        root.left.left = new Node(5);
+        root.left.right = new Node(1);
 
-
-        Solution treeBuilder = new Solution();
-        TreeNode root = treeBuilder.buildTree(preorder, inorder);
-
-        // Print the tree in inorder to verify
-        System.out.print("Inorder traversal of constructed tree: ");
-        printInorder(root);
-
+        Solution sol = new Solution();
+        System.out.println(sol.sumNums(root)); // Output: 1026
     }
 }
