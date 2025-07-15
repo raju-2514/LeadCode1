781deb28ddc1fc13b3a415acbabf306198abf187
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-07-15 23:15:40 +0600

    Add files via upload

--- src/ListNode.java
+++ src/ListNode.java
@@ -1,13 +1,7 @@
-package February.AddTwoNumber;
+package July.ReorderedList;
 
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(int val, ListNode next) {
-        this.val = val; this.next = next;
-    }
-
+    ListNode(int x) { val = x; }
 }
