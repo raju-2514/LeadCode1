fb1abce0c98d6078459aa4f41bd07426b335ae86
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-07-16 21:37:13 +0600

    Add files via upload

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,31 @@
-package June.HASHMAP;
+package July.BinaryTreePreORderTraversal;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) return null;
+        List<Integer> result = new ArrayList<>();
+        preorder(root,result);
+        return result;
+    }
+
+    public static void preorder(TreeNode node, List<Integer> result){
+       if(node==null) return;
+       result.add(node.val);
+       preorder(node.left,result);
+       preorder(node.right,result);
+    }
+
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
 
-            HashMap<String, String> map=new HashMap<String,String>();
+        Solution pt = new Solution();
+        List<Integer> output = pt.preorderTraversal(root);
 
-            map.put("A","B");
-            map.put("C","D");
-            map.put("E","F");
-            map.put("G","H");
-            map.put("I","J");
-            System.out.println(map);
-        for (String i : map.keySet()) {
-            System.out.println(i);
-        }
+        System.out.println(output);
     }
 }
