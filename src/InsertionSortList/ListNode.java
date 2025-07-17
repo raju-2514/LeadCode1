09428a0b5d9c5671b85e686aeb7cbd8106090d31
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-07-18 02:57:55 +0600

    Add files via upload

---
+++ src/InsertionSortList/ListNode.java
@@ -0,0 +1,17 @@
+package July.InsertionSortList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(){}
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
