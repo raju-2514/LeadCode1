d376dbf2fe984135c3ea253bde576e7b81f10931
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-07-23 10:51:35 +0600

    Add files via upload

---
+++ src/TernayOperation.java
@@ -0,0 +1,12 @@
+package July;
+
+public class TernayOperation {
+    public static void main(String args[]) {
+        int i = 20;
+        int j = 55;
+        int z = 0;
+        z = i < j ? i : j;
+        System.out.println("The value assigned is " + z);
+    }
+
+}
