33be82a7b84aa7959c92ecd2629e8d7f58c54c6e
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-07-29 20:14:10 +0600

    Add files via upload

---
+++ src/school-website/src/test/java/com/school/schoolwebsite/SchoolWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.schoolwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
