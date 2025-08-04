15354bf9a27da207c9a864ead61b34eaf9c00796
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-04 22:08:05 +0600

    Add files via upload

---
+++ src/FactorialsTrainingZeros.java
@@ -0,0 +1,16 @@
+package August;
+
+public class FactorialsTrainingZeros {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n > 0) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
