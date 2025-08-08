b64c700e577fb153ca29437fde66c5b42b4f5a4f
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-08 21:34:54 +0600

    Add files via upload

---
+++ src/SecondHighestSalary.java
@@ -0,0 +1,7 @@
+package August;
+
+public class SecondHighestSalary {
+//    SELECT MAX(salary) AS SecondHighestSalary
+//    FROM Employee
+//    WHERE salary < (SELECT MAX(salary) FROM Employee);
+}
