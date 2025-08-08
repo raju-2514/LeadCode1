91784c7d7a3df1b5657ef9c9437c2c46077584ce
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-08 21:28:47 +0600

    Add files via upload

---
+++ src/CombineTwoTables.java
@@ -0,0 +1,18 @@
+package August;
+
+public class CombineTwoTables {
+//    SELECT firstName, lastName, city, state
+//    FROM Person
+//    LEFT JOIN Address USING (personId);
+
+
+//    SELECT
+//    p.firstName,
+//    p.lastName,
+//    a.city,
+//    a.state
+//    FROM Person p
+//    LEFT JOIN Address a
+//    ON p.personId = a.personId;
+
+}
