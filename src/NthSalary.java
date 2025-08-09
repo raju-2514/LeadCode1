5d44cd52e269ce7d45c2a79474f036b984e7f17d
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-09 23:45:15 +0600

    Add files via upload

---
+++ src/NthSalary.java
@@ -0,0 +1,21 @@
+package August;
+
+public class NthSalary {
+//    CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+//    BEGIN
+//    DECLARE result INT;
+//
+//    SET N = N - 1; -- Convert to zero-based index for OFFSET
+//
+//    SELECT salary INTO result
+//    FROM (
+//            SELECT DISTINCT salary
+//                    FROM Employee
+//                    ORDER BY salary DESC
+//    ) AS salaries
+//    LIMIT 1 OFFSET N;
+//
+//    RETURN result;
+//    END
+
+}
