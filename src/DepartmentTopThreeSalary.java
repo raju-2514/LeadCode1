6aaa79c5ff1ced1bdc3f2ab6d33e8de208aa8c6b
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-11 07:44:38 +0600

    Add files via upload

---
+++ src/DepartmentTopThreeSalary.java
@@ -0,0 +1,21 @@
+package August;
+
+public class DepartmentTopThreeSalary {
+//    SELECT
+//    d.name AS Department,
+//    e.name AS Employee,
+//    e.salary AS Salary
+//    FROM (
+//                    SELECT
+//                            id,
+//                    name,
+//                    salary,
+//                    departmentId,
+//                    DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+//    FROM Employee
+//) e
+//    JOIN Department d
+//    ON e.departmentId = d.id
+//    WHERE e.salary_rank <= 3;
+
+}
