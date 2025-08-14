a58e95d8480e6e96c46a515e7137d2c2fcf741b9
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-14 08:38:55 +0600

    Add files via upload

--- src/NUmbersof1Bits.java
+++ src/NUmbersof1Bits.java
@@ -1,4 +1,17 @@
 package August;
 
 public class NUmbersof1Bits {
+    public static int hammingWeight(int n){
+        int count=0;
+       while(n !=0){
+           count+=(n & 1);
+           n>>>=1;
+       }
+       return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(11));
+        System.out.println(hammingWeight(128));
+    }
 }
