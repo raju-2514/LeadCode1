936b49f63b6305ffe4c6be2921a3f67ee7611eff
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-25 23:29:08 +0600

    Add files via upload

--- BitwiseANDofNumberRange.java
+++ BitwiseANDofNumberRange.java
@@ -1,4 +1,21 @@
 package August;
 
 public class BitwiseANDofNumberRange {
+    public static int rangeBitwiseAnd(int left, int right) {
+          int shift =0;
+
+          while(left<right){
+              left>>=1;
+              right>>=1;
+
+              shift++;
+          }
+        return left<<shift;
+    }
+
+    public static void main(String[] args) {
+        int a=15;
+        int b=48;
+        System.out.println(rangeBitwiseAnd(a,b));
+    }
 }
