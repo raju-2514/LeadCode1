654e9410bb2f8d0d5e1d0f4ee8b757687068d93a
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-08-27 12:15:34 +0600

    Add files via upload

---
+++ RemoveElement.java
@@ -0,0 +1,24 @@
+package August.RemoveElementFromLinkList;
+
+public class RemoveElement {
+public ListNode removeElements(ListNode head,int val){
+
+    ListNode dummy=new ListNode(-1);
+    dummy.next=head;
+
+    ListNode current=dummy;
+
+    while (current.next!=null){
+        if(current.next.val==val){
+            current.next=current.next.next;
+        }
+
+        else{
+            current=current.next;
+        }
+
+    }
+
+    return dummy.next;
+}
+}
