d6c66f25b01f88e9234533b0e306bffcdadc1eea
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-10-22 23:28:53 +0600

    Add files via upload

--- ShortestPalindrome.java
+++ ShortestPalindrome.java
@@ -1,40 +1,36 @@
-package September;
+package October;
 
 public class ShortestPalindrome {
     public String shortestPalindrome(String s) {
-        if (s == null || s.length() <= 1) return s;
-
         String rev = new StringBuilder(s).reverse().toString();
         String combined = s + "#" + rev;
-
         int[] lps = computeLPS(combined);
 
-        // longest palindrome prefix length
-        int longestPrefix = lps[combined.length() - 1];
-
-        // suffix to add in front
-        String suffix = s.substring(longestPrefix);
-        String prefixToAdd = new StringBuilder(suffix).reverse().toString();
-
-        return prefixToAdd + s;
+        int palindromeLength = lps[combined.length() - 1];
+        String toAdd = rev.substring(0, s.length() - palindromeLength);
+        return toAdd + s;
     }
 
     private int[] computeLPS(String str) {
         int n = str.length();
         int[] lps = new int[n];
-        int len = 0; // length of the previous longest prefix suffix
+        int len = 0;
 
-        for (int i = 1; i < n; ) {
+        for (int i = 1; i < n; i++) {
+            while (len > 0 && str.charAt(i) != str.charAt(len)) {
+                len = lps[len - 1];
+            }
             if (str.charAt(i) == str.charAt(len)) {
-                lps[i++] = ++len;
-            } else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else {
-                    lps[i++] = 0;
-                }
+                len++;
+                lps[i] = len;
             }
         }
         return lps;
+    }
+
+    public static void main(String[] args) {
+        ShortestPalindrome sp = new ShortestPalindrome();
+        System.out.println(sp.shortestPalindrome("aacecaaa")); // Output: aaacecaaa
+        System.out.println(sp.shortestPalindrome("abcd"));     // Output: dcbabcd
     }
 }
