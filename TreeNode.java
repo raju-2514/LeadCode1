5d1de0c8f6dd035d15d8c0efee205bb89eef840d
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2025-12-05 01:18:32 +0600

    Add files via upload

--- TreeNode.java
+++ TreeNode.java
@@ -1,16 +1,17 @@
-package August.BinaryTreeRightSideView;
+package December.CountCompleteTreeNode;
 
 public class TreeNode {
-          int val;
+    int val;
     TreeNode left;
-     TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-         this.right = right;
-      }
-  }
+    TreeNode right;
+ public TreeNode(){}
+    public TreeNode(int val) {
+        this.val = val;
+    }
 
-
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
