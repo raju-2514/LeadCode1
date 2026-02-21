c0618fb615f5d0ed1d261fa6a5ad3b4a955a5078
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2026-02-21 23:33:55 +0600

    Add files via upload

---
+++ DeleteNodeintheLinkedList/DeleteNode.java
@@ -0,0 +1,15 @@
+package February.DeleteNodeintheLinkedList;
+
+public class DeleteNode {
+    public static void deleteNode(ListNode node){
+        if(node ==null){
+            return;
+        }
+
+       node.val=node.next.val;
+
+        node.next=node.next.next;
+
+
+    }
+}
