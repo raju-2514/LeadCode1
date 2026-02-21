39def440bd1b1c56528e2b08226d206c697e6e0e
Md.Raju Biswas <91734699+raju-2514@users.noreply.github.com>
2026-02-21 23:33:22 +0600

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,18 @@
+package February.DeleteNodeintheLinkedList;
+
+public class ListNode {
+    int val;
+    ListNode left;
+    ListNode right;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode left, ListNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
